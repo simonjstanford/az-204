a25ab5fbdb07501c505502aeda149c5abfd80158
Simon Stanford <simon.stanford@domino-uk.com>
2022-08-22 11:02:24 +0100

    Finished ACR

--- Implement IaaS Solutions - ACR/create_acr.sh
+++ Implement IaaS Solutions - ACR/create_acr.sh
@@ -27,3 +27,21 @@ az acr login --name $ACR_NAME
 ACR_LOGINSERVER=$(az acr show --name $ACR_NAME --query loginServer --output tsv)
 echo $ACR_LOGINSERVER
 
+# Tag the container image using the login server name
+docker tag webappimage:v1 $ACR_LOGINSERVER/webappimage:v1
+docker image ls $ACR_LOGINSERVER/webappimage:v1
+docker image ls
+
+# Push the image to the azure container registry
+docker push $ACR_LOGINSERVER/webappimage:v1
+
+# Show the repos in the ACR
+az acr repository list --name $ACR_NAME --output table
+az acr repository show-tags --name $ACR_NAME --repository webappimage --output table
+
+
+####
+# Alternatively we could build the container in Azure instead of locally using Tasks
+###
+az acr build --image "webappimage:v1-acr-task" --registry $ACR_NAME .
+az acr repository show-tags --name $ACR_NAME --repository webappimage --output table
