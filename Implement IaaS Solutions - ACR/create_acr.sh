ca600d032d2e6b058c4cce90e627175c18fa1437
Simon Stanford <simon.stanford@domino-uk.com>
2022-08-22 14:40:20 +0100

    Added ACI

--- Implement IaaS Solutions - ACR/create_acr.sh
+++ Implement IaaS Solutions - ACR/create_acr.sh
@@ -22,7 +22,8 @@ docker rm webapp
 # create the azure container registry
 az login
 ACR_NAME='simonpsdemoacr'
-az acr create --resource-group RND-RG-DCI-TRAINING --name $ACR_NAME --sku Standard
+RESOURCE_GROUP='RND-RG-DCI-TRAINING'
+az acr create --resource-group $RESOURCE_GROUP --name $ACR_NAME --sku Standard
 az acr login --name $ACR_NAME
 ACR_LOGINSERVER=$(az acr show --name $ACR_NAME --query loginServer --output tsv)
 echo $ACR_LOGINSERVER
@@ -45,3 +46,64 @@ az acr repository show-tags --name $ACR_NAME --repository webappimage --output t
 ###
 az acr build --image "webappimage:v1-acr-task" --registry $ACR_NAME .
 az acr repository show-tags --name $ACR_NAME --repository webappimage --output table
+
+
+# create a container instance that uses an example container from microsoft
+az container create \
+   --resource-group $RESOURCE_GROUP \
+   --name simon-psdemo-hello-world-cli \
+   --dns-name-label simon-psdemo-hello-world-cli \
+   --image mcr.microsoft.com/azuredocs/aci-helloworld \
+   --ports 80
+
+az container show --resource-group $RESOURCE_GROUP --name simon-psdemo-hello-world-cli
+URL=$(az container show --resource-group $RESOURCE_GROUP --name simon-psdemo-hello-world-cli --query ipAddress.fqdn | tr -d '"')
+echo $URL
+
+#now create a container instance out of the container we built at the start
+ACR_REGISTRY_ID=$(az acr show --name $ACR_NAME --query id --output tsv)
+ACR_LOGINSERVER=$(az acr show --name $ACR_NAME --query loginServer --output tsv)
+
+echo "ACR ID: $ACR_REGISTRY_ID"
+echo "ACR Login Server: $ACR_LOGINSERVER"
+
+# Create a service principal and get the password and ID, this will allow Azure Container Instances to pull image from our ACR
+SP_NAME=acr-service-principal
+SP_PASSWD=$(az ad sp create-for-rbac \
+    --name http://$ACR_NAME-pull \
+    --scopes $ACR_REGISTRY_ID \
+    --role acrpull \
+    --query password \
+    --output tsv)
+
+SP_APPID=$(az ad sp list \
+    --display-name http://$ACR_NAME-pull \
+    --query '[].appId' \
+    --output tsv)
+
+echo "Service principal ID: $SP_APPID"
+echo "Service principal password: $SP_PASSWD"
+
+
+# Create the container in ACI, this will pull our image named
+az container create \
+    --resource-group $RESOURCE_GROUP \
+    --name simon-psdemo-webapp-cli \
+    --dns-name-label simon-psdemo-webapp-cli \
+    --ports 80 \
+    --image $ACR_LOGINSERVER/webappimage:v1 \
+    --registry-login-server $ACR_LOGINSERVER \
+    --registry-username $SP_APPID \
+    --registry-password $SP_PASSWD 
+
+az container show --resource-group $RESOURCE_GROUP --name simon-psdemo-webapp-cli
+URL=$(az container show --resource-group $RESOURCE_GROUP --name simon-psdemo-webapp-cli --query ipAddress.fqdn | tr -d '"')
+echo $URL
+curl $URL
+
+az container logs --resource-group $RESOURCE_GROUP --name simon-psdemo-webapp-cli
+
+az container delete  \
+    --resource-group $RESOURCE_GROUP \
+    --name simon-psdemo-webapp-cli \
+    --yes
