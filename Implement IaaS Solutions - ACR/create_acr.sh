d04f2ae745dcbc612cf547a15f7991c06cd1b585
Simon Stanford <simon.stanford@domino-uk.com>
2022-08-22 08:37:11 +0100

    Started creation of ACR

--- Implement IaaS Solutions - ACR/create_acr.sh
+++ Implement IaaS Solutions - ACR/create_acr.sh
@@ -18,3 +18,12 @@ curl http://localhost:8080
 # tidy up
 docker stop webapp
 docker rm webapp
+
+# create the azure container registry
+az login
+ACR_NAME='simonpsdemoacr'
+az acr create --resource-group RND-RG-DCI-TRAINING --name $ACR_NAME --sku Standard
+az acr login --name $ACR_NAME
+ACR_LOGINSERVER=$(az acr show --name $ACR_NAME --query loginServer --output tsv)
+echo $ACR_LOGINSERVER
+
